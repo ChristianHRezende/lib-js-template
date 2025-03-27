1e3e8e67646b043247c9b50cad2b757d70ba6165
Christian Rezende <christian.rezende@hsl.org.br>
2025-03-27 15:26:47 -0300

    chore: add libs

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'node',
+    include: ['src/**/*.test.ts'],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'html'],
